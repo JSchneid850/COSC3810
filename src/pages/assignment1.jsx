eb0a93b920edc66f0cc6c30cb296c691b9431b88
fyrelik <freebornrjohn@gmail.com>
2023-03-28 13:03:04 -0500

    maybe assignment1

--- src/pages/assignment1.jsx
+++ src/pages/assignment1.jsx
@@ -1,6 +1,12 @@
 
+import Pdfview from '../components/pdfview.jsx';
+
 const Assignment = () => {
-    return <h1> Assignment 1 </h1>
+    return (
+    <div>
+    <Pdfview pdf="C:\Users\johni\Documents\COSC3810\public\sillybus.pdf" />
+    </div>
+    );
 }
 
 export default Assignment;

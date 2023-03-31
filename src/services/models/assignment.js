3fd3220b63801e1617038d4b409ee28cd21a0246
Josh Schneider <joshschneider145@gmail.com>
2023-03-31 13:15:00 -0500

    I really have to stop commiting to main

--- src/services/models/assignment.js
+++ src/services/models/assignment.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const AssignmentSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
+  data : { type: String, required: true },
   dueDate: { type: Date, required: true },
   completed: { type: Boolean, default: false }
 });

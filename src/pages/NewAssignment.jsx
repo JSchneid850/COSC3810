8034bf73d32408cd6115400ac4a7812bae02cca4
fyrelik <freebornrjohn@gmail.com>
2023-04-26 11:50:36 -0500

    Posting post is now added

--- src/pages/NewAssignment.jsx
+++ src/pages/NewAssignment.jsx
@@ -2,15 +2,19 @@ import { useState, useEffect} from "react";
 import { Card, Button } from "react-bootstrap";
 import Assignment from "../components/Assignment";
 import styled from "styled-components";
+import "../Layout.css";
+import Axios from "axios";
+
 
 const NewAssignment = () => {
-    return (    
-        <div>
-            <h1 className="px-2" style = {{color: "#56a8d0", lineHeight: 2}}>New Assignment</h1>
-            <TabGroup />
-        </div>
-    )
-
+  return (    
+    <div>
+      <h1 className="px-2" style = {{color: "#56a8d0", lineHeight: 2}}>New Assignment</h1>
+      <div style={{padding: "30px"}}>
+          <TabGroup />
+      </div>    
+    </div>
+  )
 }
 
 const Tab = styled.button`
@@ -46,12 +50,92 @@ function TabGroup() {
             </Tab>
           ))}
         </div>
-        <p />
-        <p> Your payment selection: {active} </p>
+        <div>
+            {conditional(active)}
+        </div>
       </>
     );
   }
-  const types = ["Post", "Assignment"];
-  
+
+function conditional(tab){
+  if (tab === "Post"){
+    return (
+      <form onSubmit={handlePost}>
+        <table>
+        <span>Class: </span>
+          <select id="class">
+            <option value="COSC2100">COSC2100</option>
+            <option value="COSC3810">COSC3810</option>
+            <option value="COSC4920">COSC4910</option>
+          </select>
+          <tr>
+            <input type="text" class="title" placeholder="Title" id="title"/>
+          </tr>
+          <tr>
+            <textarea class="textbox" id="post"/>
+          </tr>
+        </table>
+
+        <input type="submit" value="Submit"  />
+      </form>
+    )
+  }
+  if (tab === "Assignment"){
+    return (
+      <form onSubmit={handleAssignment}>
+        <table>
+          <span>Class: </span>
+            <select>
+              <option value="COSC2100">COSC2100</option>
+              <option value="COSC3810">COSC3810</option>
+              <option value="COSC4920">COSC4910</option>
+            </select>
+
+            <tr>
+              <input type="text" class="title" placeholder="Title" id="title"/>
+            </tr>
+        </table>
+        <input type="submit" value="Submit" />
+      </form>
+    )
+  }
+}
+
+const types = ["Post", "Assignment"];
+
+const handlePost = async (event) =>{
+  event.preventDefault();
+  const d = new Date();
+  const url = 'localhost:8080/api/post';
+  console.log(event.target.class.value);
+  const data = {
+    class: event.target.class.value,
+    title: event.target.title.value,
+    post: event.target.post.value,
+    time: d.toString()
+  }
+  console.log(data);
+  await Axios.post(url, data).then((response) => {
+    console.log(response);
+  }).catch((err) => {
+      if(err.response){
+        console.log(err.response);
+        console.log("server responded");
+      }else if(err.request){
+        console.log("network error");
+      }else{
+        console.log(err);
+      }
+  })
+}
+
+
+
+const handleAssignment = event => {
+  event.preventDefault();
+
+}
+
+
 
 export default NewAssignment;

cfa8e37b64a7659b3a7ec685006094a7b8606dc2
fyrelik <freebornrjohn@gmail.com>
2023-05-01 16:52:55 -0500

    Started pull framework

---
+++ frontend/src/components/PullAss.jsx
@@ -0,0 +1,11 @@
+import Axios from "axios";
+
+function PullAss(Assigner){
+    return (
+        axios.get()
+        .then(function(response){
+            
+        })
+    )
+}
+export default PullAss;

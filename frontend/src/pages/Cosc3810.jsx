8bc2e35eeca4ef6ca122b366b4571de9ab846b4a
Teague <teague.mcginn@marquette.edu>
2023-05-02 12:02:13 -0500

    updated class pages, added reload on submissions

--- frontend/src/pages/Cosc3810.jsx
+++ frontend/src/pages/Cosc3810.jsx
@@ -1,39 +1,25 @@
-import { useState, useEffect} from "react";
-import { Card, Button } from "react-bootstrap";
-import Assignment from "../components/Assignment";
+import "../Class.css";
 
 function Cosc3810() {
 
-  const [showAssignment, setShowAssignment] = useState(false);
-  const [viewportHeight, setViewportHeight] = useState(window.innerHeight);
-
-  useEffect(() => {
-    const handleResize = () => setViewportHeight(window.innerHeight);
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  const handleButtonClick = () => {
-    setShowAssignment(true);
-  };
-
-  const cardMinHeight = viewportHeight - 120; // header + footer height
-
   return (
     <>
-      <h1 className="p-3">cosc3810: Assignment</h1>
-      <Card style={{ width: "100%", minHeight: cardMinHeight }}>
-        <Card.Body>
-          <Card.Title>Assignment 1 (Due: 3/31/23)</Card.Title>
-          {showAssignment ? (
-            <Assignment pdfUrl="https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf" />
-          ) : (
-            <Button variant="primary" onClick={handleButtonClick}>
-              Show Assignment
-            </Button>
-          )}
-        </Card.Body>
-      </Card>
+      <h1 className="p-3">COSC 3810</h1>
+      <div className="float-container">
+
+        <div className="float-assignment">
+          <div className="held">
+            <h2>Assignments</h2>
+          </div>
+        </div>
+
+        <div className="float-post">
+          <div className="held">
+            <h2>Posts</h2>
+          </div>
+        </div>
+
+      </div>     
     </>
   );
 }

619ce7df98007e65610063b8d441af6368d3f71b
Josh Schneider <joshschneider145@gmail.com>
2023-05-03 11:19:02 -0500

    4920 is now 4910

--- frontend/src/pages/Cosc3810.jsx
+++ frontend/src/pages/Cosc3810.jsx
@@ -1,6 +1,22 @@
 import "../Class.css";
 
 function Cosc3810() {
+  const [assignments, setAssignments] = useState([]);
+    useEffect(() => {
+      axios.get("http://localhost:8080/api/assignment/class/COSC3810")
+      .then(function(response){
+        setAssignments(response.data);
+      })
+    }, []);
+
+  const [posts, setPosts] = useState([]);
+    useEffect(() => {
+      axios.get("http://localhost:8080/api/post/class/COSC3810")
+      .then(function(response){
+        setPosts(response.data);
+      })
+    }, []);
+
 
   return (
     <>
@@ -10,12 +26,16 @@ function Cosc3810() {
         <div className="float-assignment">
           <div className="held">
             <h2>Assignments</h2>
+            <hr/>
+            <Assignmentwrapper assignments={assignments}/>
           </div>
         </div>
 
         <div className="float-post">
           <div className="held">
             <h2>Posts</h2>
+            <hr/>
+            <Postwrapper posts={posts}/>
           </div>
         </div>
 
--- frontend/src/pages/Cosc4920.jsx
+++ frontend/src/pages/Cosc4920.jsx
@@ -1,21 +1,40 @@
 import "../Class.css"
 
 function Cosc4920(){
+  const [assignments, setAssignments] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:8080/api/assignment/class/COSC4910")
+    .then(function(response){
+      setAssignments(response.data);
+    })
+  }, []);
+
+const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:8080/api/post/class/COSC4910")
+    .then(function(response){
+      setPosts(response.data);
+    })
+  }, []);
 
     return (
       <>
-      <h1 className="p-3">COSC 4920</h1>
+      <h1 className="p-3">COSC 4910</h1>
       <div className="float-container">
 
         <div className="float-assignment">
           <div className="held">
             <h2>Assignments</h2>
+            <hr/>
+            <Assignmentwrapper assignments={assignments}/>
           </div>
         </div>
 
         <div className="float-post">
           <div className="held">
             <h2>Posts</h2>
+            <hr/>
+            <Postwrapper posts={posts}/>
           </div>
         </div>
 
